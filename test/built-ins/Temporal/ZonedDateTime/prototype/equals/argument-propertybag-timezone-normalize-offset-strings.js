4b0f5203268d9195e7f3be26feffba0e81bae08e
Justin Grant <justingrant@users.noreply.github.com>
2023-07-19 00:52:10 -0700

    fixup! Add ZonedDateTime tests for proposal-canonical-tz

--- test/built-ins/Temporal/ZonedDateTime/prototype/equals/argument-propertybag-timezone-normalize-offset-strings.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/equals/argument-propertybag-timezone-normalize-offset-strings.js
@@ -3,31 +3,38 @@
 
 /*---
 esid: sec-temporal.zoneddatetime.prototype.equals
-description: Offset time zone identifiers are compared using their normal form, ignoring syntax differences like trailing zeroes
+description: Offset time zone identifiers are compared using their normal form, ignoring syntax differences in offset strings
 features: [Temporal, time-zone-canonicalization]
 ---*/
 
-const instance = new Temporal.ZonedDateTime(0n, "+00:00");
+const tests = [
+  { idToTest: "+0000", description: "colon-less" },
+  { idToTest: "+00", description: "hours-only" }
+];
 
-const trailingZeroesId = "+00:00:00.000000000";
-const bag1 = { year: 1970, monthCode: "M01", day: 1, timeZone: trailingZeroesId };
-assert.sameValue(instance.equals(bag1), true, "Offset time zones are equal despite trailing zeroes in property bag argument");
+for (const test of tests) {
+  const {idToTest, description} = test;
+  const instance = new Temporal.ZonedDateTime(0n, "+00:00");
 
-const str = "1970-01-01[+00:00:00.000000000]";
-assert.sameValue(instance.equals(str), true, "Offset time zones are equal despite trailing zeroes in ISO string argument");
-
-const getPossibleInstantsFor = (pdt) => [Temporal.Instant.from(`${pdt.toString()}Z`)]
-const plainObj = { id: trailingZeroesId, getPossibleInstantsFor, getOffsetNanosecondsFor: () => 0 };
-const bag2 = { year: 1970, monthCode: "M01", day: 1, timeZone: plainObj };
-assert.sameValue(instance.equals(bag2), true, "Offset time zones are equal despite trailing zeroes in plain object time zone ID");
-
-class CustomTimeZone extends Temporal.TimeZone {
-  constructor() {
-    super(trailingZeroesId);
+  const bag1 = { year: 1970, monthCode: "M01", day: 1, timeZone: idToTest };
+  assert.sameValue(instance.equals(bag1), true, `Offset time zones are equal despite ${description} syntax in property bag argument`);
+  
+  const str = "1970-01-01[+00:00]";
+  assert.sameValue(instance.equals(str), true, `Offset time zones are equal despite ${description} syntax in ISO string argument`);
+  
+  const getPossibleInstantsFor = (pdt) => [Temporal.Instant.from(`${pdt.toString()}Z`)]
+  const plainObj = { id: idToTest, getPossibleInstantsFor, getOffsetNanosecondsFor: () => 0 };
+  const bag2 = { year: 1970, monthCode: "M01", day: 1, timeZone: plainObj };
+  assert.sameValue(instance.equals(bag2), true, `Offset time zones are equal despite ${description} syntax in plain object time zone ID`);
+  
+  class CustomTimeZone extends Temporal.TimeZone {
+    constructor() {
+      super(idToTest);
+    }
+    get id() { return idToTest; }
   }
-  get id() { return trailingZeroesId; }
+  const customTimeZoneInstance = new CustomTimeZone();
+  assert.sameValue(customTimeZoneInstance.id, idToTest);
+  const bag3 = { year: 1970, monthCode: "M01", day: 1, timeZone: customTimeZoneInstance };
+  assert.sameValue(instance.equals(bag3), true, `Offset time zones are equal despite ${description} syntax in custom object time zone ID`);
 }
-const customTimeZoneInstance = new CustomTimeZone();
-assert.sameValue(customTimeZoneInstance.id, trailingZeroesId);
-const bag3 = { year: 1970, monthCode: "M01", day: 1, timeZone: customTimeZoneInstance };
-assert.sameValue(instance.equals(bag3), true, "Offset time zones are equal despite trailing zeroes in custom object time zone ID");
